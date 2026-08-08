71be9a1edc4df187592e1ba371066ae7157a0820
Nathan Anderson <andern26@classwork.engr.oregonstate.edu>
2026-08-08 13:46:31 -0700

    update delete and creat for m:n created in pl. not connected yet

--- PL.sql
+++ PL.sql
@@ -1,7 +1,7 @@
 -- ---------------------------------------------------------
 -- Project Lift Log — Group 16
 -- Nathan Anderson, Logan Jordan
--- CS340 Project Step 4
+-- CS340 Project Step 5
 -- PL/SQL procedures
 -- ---------------------------------------------------------
 -- This procedure demonstrates a CUD operation working against
@@ -19,5 +19,72 @@ BEGIN
     INNER JOIN Users ON Goals.userID = Users.userID
     WHERE Users.username = 'jsmith'
       AND Goals.goalType = 'Bodyweight';
+END //
+DELIMITER ;
+-- ---------------------------------------------------------
+-- WORKOUT EXERCISES
+-- ---------------------------------------------------------
+
+-- workout exercises create function
+
+DROP PROCEDURE IF EXISTS sp_create_workoutexercise;
+
+DELIMITER //
+CREATE PROCEDURE sp_create_workoutexercise(
+  -- psuedonames
+  IN p_workoutID INT,
+  IN p_exerciseID INT,
+  IN p_repCount INT,
+  IN p_weight DECIMAL(6,2),
+  IN p_durationSeconds INT
+)
+BEGIN
+    INSERT INTO WorkoutExercises (workoutID, exerciseID, repCount, weight, durationSeconds)
+    VALUES (
+      -- values to be inserted
+      p_workoutID,
+      p_exerciseID,
+      p_repCount,
+      p_weight,
+      p_durationSeconds
+    );
+END //
+DELIMITER ;
+
+-- workout exercises delete function
+
+DROP PROCEDURE IF EXISTS sp_delete_workoutexercise;
+
+DELIMITER //
+CREATE PROCEDURE sp_delete_workoutexercise(
+  -- psuedonames
+  IN p_workoutExerciseID INT
+)
+BEGIN
+    -- deleting a workoutExercise with correct id
+    DELETE FROM WorkoutExercises
+    WHERE workoutExerciseID = p_workoutExerciseID;
+END //
+DELIMITER ;
+
+-- workout exercises update function
+
+DROP PROCEDURE IF EXISTS sp_update_workoutexercise;
+
+DELIMITER //
+CREATE PROCEDURE sp_update_workoutexercise(
+  -- psuedonames
+  IN p_workoutExerciseID INT,
+  IN p_workoutID INT,
+  IN p_exerciseID INT,
+  IN p_repCount INT,
+  IN p_weight DECIMAL(6,2),
+  IN p_durationSeconds INT
+)
+BEGIN
+  -- update function setting all workoutexercise params to new params
+  UPDATE WorkoutExercises
+  SET workoutID = p_workoutID, exerciseID = p_exerciseID, repCount = p_repCount, weight = p_weight, durationSeconds = p_durationSeconds
+  WHERE workoutExerciseID = p_workoutExerciseID;
 END //
 DELIMITER ;
